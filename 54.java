7e13383806abe0ca92a96c6bf6807ad1b34615e7
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-04 17:02:44 +0530

    even number less than o equal to N

---
+++ 54.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n%2==1)
+		System.out.println(n-1);
+		else
+		System.out.println(n);
+	}
+}
