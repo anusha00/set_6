db4ed5a4c7d942538db3dc627277531abb24ee0c
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-04 17:05:20 +0530

    Update and rename 54.java to Even_num.java

--- Even_num.java
+++ Even_num.java
@@ -2,15 +2,15 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Ideone
+class Even_num
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
-		if(n%2==1)
-		System.out.println(n-1);
+		int num=s.nextInt();
+		if(num%2==1)
+		System.out.println(num-1);
 		else
-		System.out.println(n);
+		System.out.println(num);
 	}
 }
